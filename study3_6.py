043ae0f77b3fe1f515c07640644da979cd90e055
Mykola Tabashniuk <mtabashniuk@outlook.com>
2024-05-08 18:24:25 +0300

    Вирішив завдання 5 та 6

---
+++ study3_6.py
@@ -0,0 +1,5 @@
+a = float(input("Введіть значеняя 1 числа:"))
+b = float(input("Введіть значеняя 2 числа:"))
+c = float(input("Введіть значеняя 3 числа:"))
+arithmetic_mean = (a + b + c) / 3
+print("Середнє арифметичне цих чисел", arithmetic_mean)
